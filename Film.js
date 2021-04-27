e737a1b184b8b9ea2dad3ddc9402a19ca8d26b81
Saadovski <73020516+Saadovski@users.noreply.github.com>
2021-04-27 20:30:04 +0200

    classes de bases sauf preferences

---
+++ Film.js
@@ -0,0 +1,12 @@
+class Film {
+  constructor(title, year, country, actors, genres, synopsys, poster, rating) {
+    this.title = title;
+    this.year = year;
+    this.country = country;
+    this.actors = actors;
+    this.genres = genres;
+    this.synopsys = synopsys;
+    this.poster = poster;
+    this.rating = rating;
+  }
+}
---
+++ User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+    this.Preferences = new Preferences();
+  }
+
+  update_preferences(film, choice){
+
+  }
+}
