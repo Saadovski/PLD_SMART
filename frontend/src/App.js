82ac7e11803a4a1864f09bb14240ccefe7b8cda3
LAPTOP-34LPD6K6\Bastien <bastien.bertholom@gmail.com>
2021-04-28 09:59:04 +0200

    Aide Mario (Non fonctionnel)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Signup_Signin from './component/signup_signin';
+import Signup from './component/signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
           <Switch>
             <Route exact path="/" component={Signup_Signin} />
+            <Route exact path="/signup" component={Signup} />
           </Switch>
         </Router>
     </div>
