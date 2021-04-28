4f697562e4390d0d0a83eb5cdd4b48a30d3cb96f
Aurelien Carpentier <aurecarp@live.fr>
2021-04-28 10:41:37 +0200

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Signin from './component/signin';
-import Signup from './component/signup';
-import Signup_Signin from './component/signup_signin';
-
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Signin from "./component/signin";
+import Signup from "./component/signup";
+import Signup_Signin from "./component/signup_signin";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Switch>
-            <Route exact path="/" component={Signup_Signin} />
-            <Route exact path="/inscription" component={Signup} />
-            <Route exact path="/connexion" component={Signin} />
-
-          </Switch>
-        </Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Signup_Signin} />
+          <Route exact path="/inscription" component={Signup} />
+          <Route exact path="/connexion" component={Signin} />
+        </Switch>
+      </Router>
     </div>
   );
 }
