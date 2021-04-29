d4eaadadf04d8c6f6eca0f7ee2e024ec013124b2
Aurelien Carpentier <aurecarp@live.fr>
2021-04-29 18:35:16 +0200

    fix css names

--- frontend/src/component/signup_signin.js
+++ frontend/src/component/signup_signin.js
@@ -1,8 +1,8 @@
 import {Link} from "react-router-dom";
 import {Button} from "react-bootstrap";
-import "../styles/Boutons.css";
-import "../styles/Textes.css";
-import "../styles/Box.css";
+import "../styles/boutons.css";
+import "../styles/textes.css";
+import "../styles/box.css";
 
 function Signup_Signin() {
 
