80647c2324634abe846902d9d086799a367cc896
Aurelien Carpentier <aurecarp@live.fr>
2021-04-30 11:25:18 +0200

    fix

--- frontend/src/component/swipe/examples/Simple.js
+++ frontend/src/component/swipe/examples/Simple.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 // import TinderCard from '../react-tinder-card/index'
-import {TinderCard} from '../react-tinder-card/index'
+import TinderCard from '../react-tinder-card/index'
 import "../../../styles/swipe.css";
 import "../../../styles/box.css";
 import "../../../styles/textes.css";
