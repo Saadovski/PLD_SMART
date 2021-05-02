044e4d25b860ecf4b49891abce043be67d570520
rfonck <62340317+rfonck@users.noreply.github.com>
2021-05-02 20:48:41 +0200

    ajout des premiers sockets

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const app = require('./app');
+var fs = require('fs');
+
+
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
@@ -37,6 +40,8 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
+
+
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
@@ -44,4 +49,97 @@ server.on('listening', () => {
   console.log('Listening on ' + bind);
 });
 
+const io = require("socket.io")(server, {cors: {origin: "*"}})
+
+// Quand un client se connecte, on le note dans la console
+
+
+var usersSocketId = {}; // user: socketId
+
+var usersGroup = {} // userId: groupeId   
+var listGroupe = []; // groupeId 
+var groupeComposition = {}; // groupeId : [userID]
+var groupeMaster = {}; // groupeId: userId 
+
+io.sockets.on('connection', (socket) => {
+
+  console.log('Un client est connecté !' + socket.id);
+
+
+  socket.on("openGroup", (data) => {
+    console.log(data);
+    // on vérifie que les champs nécéssaires sont renseignés
+    
+
+      
+    //auth
+
+
+    //récuperer les coordonnées de l'utilisateur
+
+    usersSocketId[data.id] = socket.id
+    // On vérifie que l'utilisateur n'est pas dans un autre groupe.
+
+    if(usersGroup.hasOwnProperty(data.id)){
+      oldGroupId = usersGroup[data.id]
+      if(groupeMaster[oldGroupId] === data.id){
+        delete groupeMaster[oldGroupId]
+        delete groupeComposition[oldGroupId]
+      }  
+      else{
+        groupeComposition[oldGroupId].remove(data.id)
+      }
+    }
+    // créer un groupe et l'inscrire dans les variables partagées
+    var id = '_' + Math.random().toString(36).substr(2, 9);
+    listGroupe.push(id);
+    groupeMaster[id] = data.id
+    groupeComposition[id] = [data.id]
+    console.log("id : " + id)
+    console.log("groupeMaster : " + groupeMaster[id])
+    console.log("groupeComposition : " + groupeComposition[id])
+  });
+
+  socket.on("joinGroup", (data) => {
+    console.log(data);
+    //auth
+
+
+    //récuperer les coordonnées de l'utilisateur
+
+    usersSocketId[data.id] = socket.id
+    // On vérifie que l'utilisateur n'est pas dans un autre groupe.
+
+    if(usersGroup.hasOwnProperty(data.id)){
+      oldGroupId = usersGroup[data.id]
+      if(groupeMaster[oldGroupId] === data.id){
+        delete groupeMaster[oldGroupId]
+        delete groupeComposition[oldGroupId]
+      }  
+      else{
+        groupeComposition[oldGroupId].remove(data.id)
+      }
+    }
+    // ajouter l'utilisateur dans son groupe et l'inscrire dans les variables partagées
+
+    groupeComposition[data.groupId].push(data.id)
+    console.log("id : " + data.groupId)
+    console.log("groupeMaster : " + groupeMaster[data.groupId])
+    console.log("groupeComposition : " + groupeComposition[data.groupId])
+
+    // envoyer le contenu du groupe au nouvel utilisateur
+
+    socket.emit('group', groupeComposition[data.groupId])
+  });
+
+  
+
+
+
+
+});
+
+
+
 server.listen(port);
+
