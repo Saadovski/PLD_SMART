9f8a1b3adc8b1ca101e5ed3893721fbfef51374f
jj1999 <jjacques.meldrum@gmail.com>
2021-05-03 22:32:20 +0200

    ajout resultat swipe etc

--- backend/sockets/group.js
+++ backend/sockets/group.js
@@ -6,24 +6,15 @@ exports.Group = class Group {
         this.users = [user];
         this.status = "waiting";
         this.list_films = [];
-        this.mood =[];
-        this.resultatSwipe
-
-        // {
-        //     user1: 34,
-        //     user2: 35
-        // }
-
-        // {
-        //      filmid1: 2,
-        //      filmid2: 3,
-        //      filmid3: 10,
-        // }
+        this.mood = [];
+        this.resultatSwipe = {user: 0};
+        this.countFilm = {};
         
     }
   
     addUser(username){
-      this.users.push(username)
+      this.users.push(username);
+      this.resultatSwipe[username] = 0;
     }
     removeUser(username){
         if(this.users.indexOf(username) !== -1){
